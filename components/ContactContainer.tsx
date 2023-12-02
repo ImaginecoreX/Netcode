1116425aa7a0379644d774fe7acb0049c0711e3e
Nethmina Sandaruwan <125788788+ByteBigBoss@users.noreply.github.com>
2023-12-02 18:03:58 +0530

    layout changes

--- components/ContactContainer.tsx
+++ components/ContactContainer.tsx
@@ -8,48 +8,48 @@ import Image from 'next/image';
 
 const ContactContainer = () => {
   return (
-    <div className='w-full flex items-center'>
+    <div className='w-full flex items-center sm:flex-col nsTsm:flex-col md:flex-col lg:flex-row xl:flex-row 2xl:flex-row'>
       {/* Column 1 */}
-      <div>
-        <div>
-          <div><div className='text-[38px] font-normal'>Contact</div></div>
-          <div><div className='text-[64px] font-medium'>Let’s Connect</div></div>
-          <div><div className='text-[26px] font-normal leading-[38px] w-[60%]'>Drop your queries and we will get back
+      <div className='w-fit nsTsm:flex nsTsm:flex-col nsTsm:items-center sm:flex sm:flex-col sm:items-center md:items-center lg:items-start xl:items-start 2xl:items-start'>
+        <div className='nsTsm:pb-[80px] md:pb-[80px]'>
+          <div><div className='text-[38px] font-normal nsTsm:text-center md:text-center'>Contact</div></div>
+          <div><div className='text-[64px] font-medium nsTsm:text-center md:text-center'>Let’s Connect</div></div>
+          <div className='nsTsm:w-full nsTsm:flex nsTsm:flex-col nsTsm:items-center md:flex-col md:flex md:items-center'><div className='text-[26px] font-normal leading-[38px] w-[60%] nsTsm:w-[80%] nsTsm:text-center md:w-[80%] md:text-center'>Drop your queries and we will get back
             to you as soon possible...</div></div>
         </div>
 
-        <div className='mt-[60px]'>
+        <div className='mt-[60px] nsTsm:hidden md:hidden'>
           <div><div className='text-[30px] font-medium'>Send us an email:</div></div>
           <div><div className='text-[20px] font-medium text-[#0780FF]'>teams.netcode@gmail.com</div></div>
         </div>
 
-        <div className='mt-[48px]'>
+        <div className='mt-[48px] nsTsm:hidden md:hidden'>
           <div><div className='text-[30px] font-medium'>Contact us:</div></div>
           <div><div className='text-[20px] font-medium text-[#0780FF]'>+94 74 3837 327</div></div>
         </div>
 
-        <div className='flex items-center gap-[30px] mt-[60px]'>
-          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center'><Image src={github} alt='GitHub' className='w-[26px] h-[26px]' /></div>
-          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center'><Image src={x} alt='X' className='w-[26px] h-[26px]' /></div>
-          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center'><Image src={instagram} alt='Instagram' className='w-[26px] h-[26px]' /></div>
-          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center'><Image src={facebook} alt='Facebook' className='w-[26px] h-[26px]' /></div>
-          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center'><Image src={linkedind} alt='LinkedIn' className='w-[26px] h-[26px]' /></div>
+        <div className='flex items-center gap-[30px] mt-[60px] nsTsm:hidden md:hidden'>
+          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center flex-shrink-0'><Image src={github} alt='GitHub' className='w-[26px] h-[26px] flex-shrink-0' /></div>
+          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center flex-shrink-0'><Image src={x} alt='X' className='w-[26px] h-[26px] flex-shrink-0' /></div>
+          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center flex-shrink-0'><Image src={instagram} alt='Instagram' className='w-[26px] h-[26px] flex-shrink-0' /></div>
+          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center flex-shrink-0'><Image src={facebook} alt='Facebook' className='w-[26px] h-[26px] flex-shrink-0' /></div>
+          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center flex-shrink-0'><Image src={linkedind} alt='LinkedIn' className='w-[26px] h-[26px] flex-shrink-0' /></div>
         </div>
       </div>
 
       {/* column 2 */}
-      <div className='flex flex-col gap-[40px]'>
+      <div className='flex flex-col gap-[40px] flex-1'>
         {/* first, last names */}
-        <div className='items-center flex gap-[24px]'>
+        <div className='items-center grid grid-cols-2 gap-[24px]'>
           {/* first name */}
           <div className='flex-1'>
             <div className='text-[21px] font-medium pb-[14px]'>First Name</div>
-            <input type="text" className='border-[1px] border-[#A4A4A4] bg-[#FAFAFA] rounded-[12px] h-[60px] text-[21px] placeholder:font-light box-border px-[24px]' placeholder='eg:Alex' />
+            <input type="text" className='border-[1px] border-[#A4A4A4] bg-[#FAFAFA] w-full rounded-[12px] h-[60px] text-[21px] placeholder:font-light box-border px-[24px]' placeholder='eg:Alex' />
           </div>
           {/* last name */}
           <div className='flex-1'>
             <div className='text-[21px] font-medium pb-[14px]'>Last Name</div>
-            <input type="text" className='border-[1px] border-[#A4A4A4] bg-[#FAFAFA] rounded-[12px] h-[60px] text-[21px] placeholder:font-light box-border px-[24px]' placeholder='eg: Cray' />
+            <input type="text" className='border-[1px] border-[#A4A4A4] bg-[#FAFAFA] w-full rounded-[12px] h-[60px] text-[21px] placeholder:font-light box-border px-[24px]' placeholder='eg: Cray' />
           </div>
 
         </div>
@@ -77,7 +77,23 @@ const ContactContainer = () => {
 
         <button className='w-full bg-black h-[60px] rounded-[12px] text-white text-[24px] font-semibold'>Send</button>
       </div>
+      <div className='mt-[60px] 2xl:hidden xl:hidden lg:hidden'>
+          <div><div className='text-[30px] font-medium'>Send us an email:</div></div>
+          <div><div className='text-[20px] font-medium text-[#0780FF]'>teams.netcode@gmail.com</div></div>
+        </div>
 
+        <div className='mt-[48px] 2xl:hidden xl:hidden lg:hidden'>
+          <div><div className='text-[30px] font-medium'>Contact us:</div></div>
+          <div><div className='text-[20px] font-medium text-[#0780FF]'>+94 74 3837 327</div></div>
+        </div>
+
+        <div className='flex items-center gap-[30px] mt-[60px] 2xl:hidden xl:hidden lg:hidden'>
+          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center flex-shrink-0'><Image src={github} alt='GitHub' className='w-[26px] h-[26px] flex-shrink-0' /></div>
+          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center flex-shrink-0'><Image src={x} alt='X' className='w-[26px] h-[26px] flex-shrink-0' /></div>
+          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center flex-shrink-0'><Image src={instagram} alt='Instagram' className='w-[26px] h-[26px] flex-shrink-0' /></div>
+          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center flex-shrink-0'><Image src={facebook} alt='Facebook' className='w-[26px] h-[26px] flex-shrink-0' /></div>
+          <div className='p-[17px] bg-black rounded-full BTNShadow w-fit flex items-center justify-center flex-shrink-0'><Image src={linkedind} alt='LinkedIn' className='w-[26px] h-[26px] flex-shrink-0' /></div>
+        </div>
     </div>
   )
 }

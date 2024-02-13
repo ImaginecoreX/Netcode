7d0bdc8d33881a93096d6a9abaf6125f5b802aee
ByteBigBoss <nethminas42@gmail.com>
2024-02-13 17:59:05 +0530

    new changes

--- app/case-study/[slug]/page.tsx
+++ app/case-study/[slug]/page.tsx
@@ -17,7 +17,7 @@ const CaseStudy = ({ params }: { params: { slug: string } }) => {
   const id = params;
   const route = useRouter();
   const back = () => route.back();
-  const navigateToHome = ()=>route.push('/',{scroll:true});
+  const navigateToHome = () => route.push('/', { scroll: true });
 
   const [study, setStudy] = useState<CaseStudyCardProps | any>();
   useEffect(() => {
@@ -30,126 +30,128 @@ const CaseStudy = ({ params }: { params: { slug: string } }) => {
 
   const [isFullImage, setIsFullImage] = useState(false);
   const [img, setImg] = useState<imgObjProps>();
-  useEffect(()=>{
-    if(img){
+  useEffect(() => {
+    if (img) {
       setIsFullImage(true);
     }
-  },[img]);
+  }, [img]);
 
 
   const [activeSection, setActiveSection] = useState('');
   const sidebarRefs = useRef<Record<string, HTMLDivElement | null>>({});
-    // Scroll event listener to determine the active section
-    useEffect(() => {
-      const handleScroll = () => {
-        const scrollPosition = window.scrollY;
-        const sectionIds = AllCaseStudies[0].bp.map(data => data.id.toString());
-  
-        for (const id of sectionIds) {
-          const element = sidebarRefs.current[id];
-          if (element) {
-            const offsetTop = element.offsetTop;
-            const offsetBottom = offsetTop + element.clientHeight;
-            if (scrollPosition >= offsetTop && scrollPosition <= offsetBottom) {
-              setActiveSection(id);
-              break;
-            }
+  // Scroll event listener to determine the active section
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const sectionIds = AllCaseStudies[0].bp.map(data => data.id.toString());
+
+      for (const id of sectionIds) {
+        const element = sidebarRefs.current[id];
+        if (element) {
+          const offsetTop = element.offsetTop;
+          const offsetBottom = offsetTop + element.clientHeight;
+          if (scrollPosition >= offsetTop && scrollPosition <= offsetBottom) {
+            setActiveSection(id);
+            break;
           }
         }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-      return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
-
-    const scrollToSection = (sectionId: string)=>{
-      const section = document.getElementById(sectionId);
-      if(section){
-        section.scrollIntoView({
-          behavior:"smooth",
-          block: 'start',
-        });
       }
-    }
+    };
 
-    const screenSize = useScreenSize();
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: 'start',
+      });
+    }
+  }
+
+  const screenSize = useScreenSize();
 
   return (
     <>
-    {/* go up */}
-    {!isFullImage &&  
-      <motion.div initial="hidden" animate="visible" variants={slideInFromBottom(0.6)} onClick={()=>scrollToSection(screenSize==='nsTsm'?'case-home':'overview')} className='w-[40px] h-[40px] flex hover:scale-90 transform transition-transform duration-300 cursor-pointer text-white justify-center items-center bg-black bottom-[30px] right-[30px] nsTsm:right-[16px] nsTsm:bottom-[24px] z-[500] rounded-full fixed'>
-        <ArrowUpward/>
-      </motion.div>
+      {/* go up */}
+      {!isFullImage &&
+        <motion.div initial="hidden" animate="visible" variants={slideInFromBottom(0.6)} onClick={() => scrollToSection(screenSize === 'nsTsm' ? 'case-home' : 'overview')} className='w-[40px] h-[40px] flex hover:scale-90 transform transition-transform duration-300 cursor-pointer text-white justify-center items-center bg-black bottom-[30px] right-[30px] nsTsm:right-[16px] nsTsm:bottom-[24px] z-[500] rounded-full fixed'>
+          <ArrowUpward />
+        </motion.div>
       }
-    <div className={`flex flex-col items-center scroll-smooth min-h-screen relative ${isFullImage?'max-h-screen overflow-hidden':''} `} style={{scrollBehavior:'smooth'}}>
-    {isFullImage && <FullScreenImgView data={img?.dataObj} setVisible={setIsFullImage} />}
-      <motion.div initial="hidden" animate="visible" variants={slideInFromTop(0.3)} className='frame-container ' id='case-home'>
+      <div className={`flex flex-col items-center scroll-smooth min-h-screen relative ${isFullImage ? 'max-h-screen overflow-hidden' : ''} `} style={{ scrollBehavior: 'smooth' }}>
+        {isFullImage && <FullScreenImgView data={img?.dataObj} setVisible={setIsFullImage} />}
+        <motion.div initial="hidden" animate="visible" variants={slideInFromTop(0.3)} className='frame-container ' id='case-home'>
 
-        <div className='w-full flex items-center justify-between bg-white mt-[28px]'>
-          {/* column 1 */}
-          <div className='flex items-center gap-[12px] hover:cursor-pointer' onClick={navigateToHome}>
-            <div><Image src={Netcode} alt='Netcode' className='' /></div>
-            <div className='text-[20px] font-medium text-black text-center'>Netcode</div>
-          </div>
-          {/* column 2 */}
-          <div className='hover:text-blue-500 cursor-pointer'>
-            <div onClick={back} >Go Back</div>
-          </div>
-        </div>
-      </motion.div>
-
-      <motion.div initial="hidden" animate="visible" variants={slideInFromTop(0.2)} className='bg-black w-full mt-[30px] min-h-[100px] flex justify-center items-center nsTsm:h-auto nsTsm:py-[20px]'>
-        <div className='text-white text-center text-[1.4rem] font-medium nsTsm:w-[80%]'>{study?.title}</div>
-      </motion.div>
-      
-      <motion.div initial="hidden" animate="visible" variants={slideInFromBottom(0.3)} className='frame-container '>
-
-        {/* details */}
-        <div className='mt-[60px] nsTsm:mt-[30px] flex gap-[24px] nsTsm:flex-col'>
-          {/* sidebar */}
-          <div className='w-1/6 min-h-[200px] h-auto nsTsm:hidden  box-border px-[12px] py-[20px] gap-[24px] flex flex-col max-h-screen overflow-hidden overflow-y-auto hide-scrollbar'>
-            {AllCaseStudies[0].bp.map((data, index) => (
-              <div className={`${activeSection==data.id.toString()?'border-blue-500 box-border px-[6px] text-blue-500 py-[6px] border-l-[2px]':''}`} onClick={()=>scrollToSection(data.id.toString())} key={index} id={data.id.toString() } ref={ref => sidebarRefs.current[data.id.toString()] = ref}>
-                <div className=''><strong>{data.id}:</strong> {data.title}</div>
-              </div>
-            ))}
-          </div>
-
-          {/* datablock */}
-          <div className='w-5/6 nsTsm:w-full pb-[20px] max-h-screen overflow-hidden nsTsm:max-h-none mdup:overflow-y-auto lg:overflow-y-auto xl:overflow-y-auto 2xl:overflow-y-auto   hide-scrollbar'>
-            <div className='w-full min-h-[400px]' >
-
-              <div className='mb-[30px]' id='overview'>
-                <div className='text-[2rem] font-bold'>Overview</div>
-                <div className='pt-[6px] leading-[26px]'>An University Project we have to do a industry visit at manufacturing company. With that we choose Tea industry, and we visited <strong>New Kandhagasthanna Tea Factory</strong>.</div>
-              </div>
-
-              <motion.video initial="hidden" animate="visible" variants={scaleIn(0.25)} src='' className='w-full h-full bg-black' />
-
-              {/* Diagrams */}
-              <div className='mt-[40px]'>
-                <div className='text-[2rem] font-bold'>Business Process Diagrams</div>
-                <div className='pt-[6px] leading-[26px]'>Now Let's see Hand-Drawed Business Process Diagrams (BPDs) and How they illustrate a Full Analysis of the  <strong>New Kandhagasthanna Tea Factory</strong> Production Process</div>
-              </div>
-              <div className='mt-[30px] flex flex-col gap-[20px]'>
-                {AllCaseStudies[0].bp.map((data, index) => (
-                  <div onClick={()=>setImg({id:data.id,dataObj:data})}>
-                    <DiagramCard key={index} diagram={data.diagram} id={data.id} title={data.title} description={data.description} />
-                  </div>
-                ))}
-              </div>
-
-
+          <div className='w-full flex items-center justify-between bg-white mt-[28px]'>
+            {/* column 1 */}
+            <div className='flex items-center gap-[12px] hover:cursor-pointer' onClick={navigateToHome}>
+              <div><Image src={Netcode} alt='Netcode' className='' /></div>
+              <div className='text-[20px] font-medium text-black text-center'>Netcode</div>
+            </div>
+            {/* column 2 */}
+            <div className='hover:text-blue-500 cursor-pointer'>
+              <div onClick={back} >Go Back</div>
             </div>
           </div>
+        </motion.div>
 
-        </div>
+        <motion.div initial="hidden" animate="visible" variants={slideInFromTop(0.2)} className='bg-black w-full mt-[30px] min-h-[100px] flex justify-center items-center nsTsm:h-auto nsTsm:py-[20px]'>
+          <div className='text-white text-center text-[1.4rem] font-medium nsTsm:w-[80%]'>{study?.title}</div>
+        </motion.div>
+
+        <motion.div initial="hidden" animate="visible" variants={slideInFromBottom(0.3)} className='frame-container '>
+
+          {/* details */}
+          <div className='mt-[60px] nsTsm:mt-[30px] flex gap-[24px] nsTsm:flex-col'>
+            {/* sidebar */}
+            <div className='w-1/6 min-h-[200px] h-auto nsTsm:hidden  box-border px-[12px] py-[20px] gap-[24px] flex flex-col max-h-screen overflow-hidden overflow-y-auto hide-scrollbar'>
+              {AllCaseStudies[0].bp.map((data, index) => (
+                <div className={`${activeSection == data.id.toString() ? 'border-blue-500 box-border px-[6px] text-blue-500 py-[6px] border-l-[2px]' : ''}`} onClick={() => scrollToSection(data.id.toString())} key={index} id={data.id.toString()} ref={ref => sidebarRefs.current[data.id.toString()] = ref}>
+                  <div className=''><strong>{data.id}:</strong> {data.title}</div>
+                </div>
+              ))}
+            </div>
+
+            {/* datablock */}
+            <div className='w-5/6 nsTsm:w-full pb-[20px] max-h-screen overflow-hidden nsTsm:max-h-none mdup:overflow-y-auto lg:overflow-y-auto xl:overflow-y-auto 2xl:overflow-y-auto   hide-scrollbar'>
+              <div className='w-full min-h-[400px]' >
+
+                <div className='mb-[30px]' id='overview'>
+                  <div className='text-[2rem] font-bold'>Overview</div>
+                  <div className='pt-[6px] leading-[26px]'>An University Project we have to do a industry visit at manufacturing company. With that we choose Tea industry, and we visited <strong>New Kandhagasthanna Tea Factory</strong>.</div>
+                </div>
+
+                
+                <div className="w-full h-[400px] nsTsm:h-[300px] 2xl:h-[600px] xl:h-[500px]">
+                  <motion.iframe initial="hidden" animate="visible" variants={scaleIn(0.25)} src="https://www.youtube.com/embed/LzQ0E-8MYic?rel=0" title="YouTube video" className='w-full h-full' allowFullScreen></motion.iframe>
+                </div>
+                {/* Diagrams */}
+                <div className='mt-[40px]'>
+                  <div className='text-[2rem] font-bold'>Business Process Diagrams</div>
+                  <div className='pt-[6px] leading-[26px]'>Now Let's see Hand-Drawed Business Process Diagrams (BPDs) and How they illustrate a Full Analysis of the  <strong>New Kandhagasthanna Tea Factory</strong> Production Process</div>
+                </div>
+                <div className='mt-[30px] flex flex-col gap-[20px]'>
+                  {AllCaseStudies[0].bp.map((data, index) => (
+                    <div onClick={() => setImg({ id: data.id, dataObj: data })}>
+                      <DiagramCard key={index} diagram={data.diagram} id={data.id} title={data.title} description={data.description} />
+                    </div>
+                  ))}
+                </div>
 
 
-      </motion.div>
-    </div>
-    {!isFullImage && <Footer />}
+              </div>
+            </div>
+
+          </div>
+
+
+        </motion.div>
+      </div>
+      {!isFullImage && <Footer />}
     </>
   )
 
